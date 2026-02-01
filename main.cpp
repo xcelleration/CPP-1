d6f8710716bc5e5611f74734b904c1fd99bc2098
xcelleration <abdallah.s.adam@proton.me>
2026-01-31 22:48:50 -0500

    First basic Age Verification program

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+int main() {
+    std::string input;
+    while (true) {
+        std::cout<<"Enter age: ";
+        std::cin>>input;
+        if (input=="exit") {
+            break;
+        }
+        try {
+            int age{std::stoi(input)};
+            if (age<0) {
+                std::cout<<"Negative values are not allowed\n";
+            }
+            if (age<18) {
+                std::cout<<"You're a minor\n";
+            }
+            else {
+                std::cout<<"You're a major\n";
+            }
+        }
+        catch (std::invalid_argument& e) {
+            std::cout<<"Invalid input\n";
+        }
+    }
+}
